565635fbe89279b6eb0af6aad361a8198cbc3988
dami0806 <dami0806>
2022-10-12 18:51:05 +0900

    기능전 commit

---
+++ RSPGame/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  RSPGame
+//
+//  Created by 박다미 on 2022/10/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    //변수
+    @IBOutlet weak var mainLable: UILabel!
+    @IBOutlet weak var comImgView: UIImageView!
+    @IBOutlet weak var myImgView: UIImageView!
+    
+    
+    @IBOutlet weak var comChoiceLable: UILabel!
+    @IBOutlet weak var myChoiceLable: UILabel!
+    
+    
+    //함수
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func rpsBtnTapped(_ sender: Any) {
+    
+    //함수 1개에 여러개 연결 가능
+        
+        
+    
+    }
+    
+    
+    
+    
+
+}
+
